a316e6f11c83fd70e9cc5cbbaedace4dadc009d9
psychohight <7298@holbertonstudents.com>
2023-10-13 07:56:41 -0700

    5 c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,12 +1,14 @@
-nclude <stdio.h>
+#include <stdio.h>
 
 /**
  *  * main - Entry point
  *   *
  *    * Return: Always 0 (Success)
  */
+
 int main(void)
+
 {
-	printf("with proper grammar, but the outcome is a piece of art,")	   
+	printf("with proper grammar, but the outcome is a piece of art,");	   
        	return (0);
 }
