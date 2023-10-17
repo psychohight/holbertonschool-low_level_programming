323ba2d2a6c8038269b1c82b6d0765bd7c8a53ae
psychohight <7298@holbertonstudents.com>
2023-10-17 05:20:44 -0700

    1 varia

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ int main(void)
 		printf("Last digit of %d is %d and is greater than 5\n", n, dernier);
 
 	else if ( dernier == 0)
-		printf("Last digit of %d is %d and si 0\n", n, dernier);
+		printf("Last digit of %d is %d and is 0\n", n, dernier);
 
 	else if ( dernier < 6 )
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, dernier);
