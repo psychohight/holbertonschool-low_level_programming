9e1d001f819e3cb0da7c27af3c7fb8ef6eaa730c
psychohight <7298@holbertonstudents.com>
2023-10-17 01:18:15 -0700

    0 varia

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -13,16 +13,14 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	printf("%d \n", n);
-	
 	if (n > 0)
-	printf("is positive\n", n);
+	printf("%dis positive\n", n);
 
-	else if (n == 0)
-	printf("is zero\n", n);
+	if (n == 0)
+	printf("%dis zero\n", n);
 
-	else (n < 0)
-	printf("is negative\n", n);	
+	if (n < 0)
+	printf("%dis negative\n", n);
 
 	return (0);
 }
