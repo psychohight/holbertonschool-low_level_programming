2606d9932200df9915d68eed35e0573d3c618fd8
psychohight <7298@holbertonstudents.com>
2023-10-17 04:52:41 -0700

    1 varia

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -10,18 +10,19 @@
 int main(void)
 {
 	int n;
-
+	int decimal = number % 10;
+	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
 	if ( n > 5 )
-	printf ("%d and is greater than 5\n", n);
+	printf ("%d and is greater than 5\n", decimal);
 
 	if ( n == 0)
-	printf ("%d and si 0\n", n);
+	printf ("%d and si 0\n", decimal);
 
 	else if ( n < 6 )
-	printf ("%d and is less than 6 and not 0\n", n);
+	printf ("%d and is less than 6 and not 0\n", decimal);
 
 	return (0);
 }
