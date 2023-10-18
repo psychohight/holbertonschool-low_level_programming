88c0859e884ea8ce5f98a7c4563c134c70c6bd5f
psychohight <7298@holbertonstudents.com>
2023-10-18 03:38:26 -0700

    9 varia

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -10,13 +10,13 @@ int main(void)
 {
 	int number;
 
-	for (number ='0'; number <= '9'; number++)
+	for (number = '0'; number <= '9'; number++)
 	{
 		putchar(number);
-	
+
 		if (number != '9')
 		{
-			putchar(','); 
+		 	putchar(',');
 			putchar(' ');
 		}
 	}
