24a7ef6cc8eb86b17f9f909ee3715f37f991d358
psychohight <7298@holbertonstudents.com>
2023-10-18 00:57:52 -0700

    5 varia

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf( "0123456789\n" );
+
+	return (0);
+}
