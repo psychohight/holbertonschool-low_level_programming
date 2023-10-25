b8b853712380b6fa55958d07ba4d1e6986a30b0c
psychohight <7298@holbertonstudents.com>
2023-10-25 08:01:07 -0700

    5 pointer

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -4,7 +4,7 @@
 
 /**
 * rev_string - reverse
-*
+* @s: char
 * Return: Always 0.
 */
 
@@ -14,9 +14,9 @@ void rev_string(char *s)
 	int len = strlen(s);
 	int millieu = len / 2;
 	char temp;
-	
+
 	for (i = 0; i < millieu; i++)
-	
+
 	{
 		temp = s[i];
 		s[i] = s[len - i - 1];
