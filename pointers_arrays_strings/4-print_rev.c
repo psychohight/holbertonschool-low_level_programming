90c2fa3f0550b43e4a89df7b2009739240e85634
psychohight <7298@holbertonstudents.com>
2023-10-25 03:06:34 -0700

    4 pointer

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - imprimer a l'envers 
+ * 
+ * @s: mot
+ * 
+ * Return: Always 0.
+ */
+
+void print_rev(char *s)
+
+{
+	int len;
+
+	for (len = 0; s[len] != '\0'; len++);
+
+	for (len = len - 1; s[len] != '\0'; len--)
+	{
+                _putchar(s[len]);
+
+        }
+
+	_putchar('\n');
+
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,5 +1,11 @@
+#ifndef SOURCE_H_INCLUDED
+#define SOURCE_H_INCLUDED
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 char _putchar(char c);
+void print_rev(char *s);
+
+#endif
