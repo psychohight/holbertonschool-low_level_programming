f71ec220babb66cdd9d077317cc784b64d56ccdc
psychohight <7298@holbertonstudents.com>
2023-10-27 05:45:53 -0700

    0 pointer2

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * *_strcat - fusion
+ *
+ * @d: chaine
+ * @s: chaine
+ *
+ * return: 0
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int d, s;
+
+	for (d = 0; dest[d] != '\0'; d++)
+	{
+	}
+	for (s = 0; src[s] != '\0'; s++)
+	{
+		dest[d] = src[s];
+		d++;
+	}
+	
+	return (dest);
+}
