d85234410365c3ff704a4125f95e10a161513d89
psychohight <7298@holbertonstudents.com>
2023-10-27 07:10:57 -0700

    3 pointer2

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strcat - fusion
+ *
+ * @d: chaine
+ * @s: chaine
+ * @dest: pt
+ * @src: pt
+ *
+ * return: 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
+	return (0);
+}
