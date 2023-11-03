79579d7ae91d8cf323cabdc40c00a92ad9e4f134
psychohight <7298@holbertonstudents.com>
2023-11-03 06:42:55 -0700

    static

---
+++ static_libraries/9-strcpy.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+* _strcpy - copier
+* @dest: chaine
+* @src: chaine
+* Return: cpy
+*/
+char *_strcpy(char *dest, char *src)
+{
+	char *cpy = dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (cpy);
+}
