0ae096358bd2b816862092b1a1d709fcbe3e71c3
psychohight <7298@holbertonstudents.com>
2023-11-06 06:56:41 -0800

    malloc

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -21,9 +21,9 @@ char *create_array(unsigned int size, char c)
 
 	if (array == NULL)
 		return (NULL);
-	
+
 	for (i = 0; i < size; i++)
 		array[i] = c;
-	
-	return array;
+
+	return (array);
 }
---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+char *_strdup(char *str)
+{
+	if (str == NULL)
+		return NULL;
+
+
