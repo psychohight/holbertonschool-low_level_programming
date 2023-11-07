41f945b6a7b1894b91243a0513d6b7dc7f3189df
psychohight <7298@holbertonstudents.com>
2023-11-07 00:41:52 -0800

    malloc

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -3,14 +3,35 @@
 #include <stdlib.h>
 
 /**
- * main - check the code
+ * _strdup - check the code
+ *
+ * @str : nbr
  *
  * Return: Always 0.
  */
 
 char *_strdup(char *str)
 {
-	if (str == NULL)
-		return NULL;
+	int i;
+	int len = 0;
+	char *duplicate;
 
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len] != '\0')
+		len++;
+
+	duplicate = (char *)malloc(len + 1);
+
+	if (duplicate == NULL)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+		duplicate[i] = str[i];
+
+	duplicate[len] = '\0';
+
+	return (duplicate);
+}
 
