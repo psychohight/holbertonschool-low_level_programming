d7e3beb1f02f9d2e7b31bbb59ba57038b8a67c6c
psychohight <7298@holbertonstudents.com>
2023-11-13 10:26:48 +0000

    structures

---
+++ structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - check the code
+ *
+ * @name : prenom
+ * @owner : proprio
+ * @age : age
+ *
+ * Return: Always 0.
+*/
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif
