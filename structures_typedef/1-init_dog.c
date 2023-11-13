6546b394ec1aa64b79472bf9b16e65eaa4318c0c
psychohight <7298@holbertonstudents.com>
2023-11-13 13:19:26 +0000

    structure

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+#include <string.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	strcpy(d->name, name);
+
+	d->age = age;
+	d->owner = owner;
+}
