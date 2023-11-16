9b1658a5d9725046c784357f707f030920a85ade
psychohight <7298@holbertonstudents.com>
2023-11-16 09:56:13 +0000

    struc

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -10,7 +10,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	strcpy(d->name, name);
+	if (d == NULL)
+		return;
+
+	d->name = name;
 	d->age = age;
 	d->owner = owner;
 }
