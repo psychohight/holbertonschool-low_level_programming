f24c51ba1aa74b3f1480beda2a6702f2e78c7bb0
psychohight <7298@holbertonstudents.com>
2023-11-28 13:36:37 +0000

    task 0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -2,23 +2,27 @@
 #include <stdlib.h>
 #include <string.h>
 #include "lists.h"
-
+/**
+ * print_list - check code
+ *
+ * @h: nbr
+ * Return: print_list
+ */
 size_t print_list(const list_t *h)
-
 {
 	size_t count = 0;
 
-    while (h != NULL)
-    {
-    	if (h->str == NULL)
-            printf("[0] (nil)\n");
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
 
 		else
-            printf("[%d] %s\n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 
-        h = h->next;
-        count++;
-    }
+		h = h->next;
+		count++;
+	}
 
-    return count;
+	return (count);
 }
