fc4adc197d5f556b0692bcfd76ceee3b141be3ec
psychohight <7298@holbertonstudents.com>
2023-12-07 13:00:21 +0000

    task 1

--- file_io/main.h
+++ file_io/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
@@ -7,5 +8,6 @@
 #include <unistd.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif
