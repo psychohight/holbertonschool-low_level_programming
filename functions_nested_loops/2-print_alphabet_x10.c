65e4b31b3e3586a0112849fe9a2c5f321d26547a
psychohight <7298@holbertonstudents.com>
2024-02-22 14:10:47 +0100

    2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* main - print alphabet  x10
+*
+* Return: Always c.
+*/
+void print_alphabet(void)
+{
+	char c, i;
+
+	for (i = 0; i <= 9; i++)
+    {
+        for (c = 'a'; c <= 'z'; c++)
+            _putchar(c);
+    }
+	_putchar('\n');
+}
