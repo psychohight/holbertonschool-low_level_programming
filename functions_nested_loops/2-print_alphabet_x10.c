2159d02c7e49138e6aed98f8ea2123b4a882fa2a
psychohight <7298@holbertonstudents.com>
2024-02-22 14:19:24 +0100

    2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -5,14 +5,16 @@
 *
 * Return: Always c.
 */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 	char c, i;
 
 	for (i = 0; i <= 9; i++)
-    {
+	{
         for (c = 'a'; c <= 'z'; c++)
-            _putchar(c);
+			_putchar(c);
+    
+	    _putchar('\n');
     }
-	_putchar('\n');
 }
+    
