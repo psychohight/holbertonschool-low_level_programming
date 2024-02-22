10658b800c30126c796cd25d42fa9995adb91a7b
psychohight <7298@holbertonstudents.com>
2024-02-22 13:00:12 +0100

    betty

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * main - print alphabet
- *
- * Return: Always 0.
- */
+* main - print alphabet
+*
+* Return: Always c.
+*/
 void print_alphabet(void)
 {
-    char c;
+	char c;
 
-    for (c = 'a'; c <= 'z'; c++)
-        _putchar(c);
-    
-    _putchar('\n');
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
 }
