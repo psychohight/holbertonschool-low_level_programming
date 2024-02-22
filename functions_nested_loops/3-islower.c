2109dfcfaa6b5e5ca63470b5037bca1fbf162a4d
psychohight <7298@holbertonstudents.com>
2024-02-22 15:20:17 +0100

    3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+
+int _islower(int c)
+{
+    if (c >= 'a' && c <= 'z')
+        return(1);
+
+    return(0);
+
+}
