ffe91b3f364641f3b0f4a83523bacdabb23aafa4
psychohight <7298@holbertonstudents.com>
2024-02-23 17:30:08 +0100

    5-6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int _abs(int n)
+{
+    if (n < 0)
+        return(-n);
+    else
+        return(n);
+
+    return(0);
+}
