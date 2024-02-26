0532d2088720a0c4ef26734a6ecaf91ff49299ce
psychohight <7298@holbertonstudents.com>
2024-02-26 14:38:51 +0100

    7

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -9,5 +9,15 @@
 
 void print_diagonal(int n)
 {
-    
+    int i;
+    int j;
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < i; j++)
+            _putchar(' ');
+        
+        _putchar('\\');
+        _putchar('\n');
+    }
 }
