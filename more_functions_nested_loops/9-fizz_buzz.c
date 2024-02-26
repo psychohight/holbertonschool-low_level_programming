ab44970415a27f1d37a518dfd8e4646013f32d06
psychohight <7298@holbertonstudents.com>
2024-02-26 17:00:53 +0100

    9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,7 +12,10 @@ int main (void)
 
     for (n = 1 ; n <= 100 ; n++)
     {    
-        if (n % 3 == 0 && n % 5 == 0)
+        if (n == 100)
+            printf("Buzz");
+
+        else if (n % 3 == 0 && n % 5 == 0)
             printf("FizzBuzz ");
 
         else if (n % 3 == 0)
