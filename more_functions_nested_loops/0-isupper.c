2a46930dbd86eab976e447e5ad111078084b798c
psychohight <7298@holbertonstudents.com>
2024-02-26 10:54:02 +0100

    1

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,20 @@
 #include "main.h"
 #include <stdio.h>
+#include <ctype.h>
 
 /**
- * main - check the code.
- *
+ * main - isupper
+ * 
+ * @c : lettre
+ * 
  * Return: Always 0.
  */
 
 int _isupper(int c)
 {
+    if isupper(c)
+        return(1);
     
+    else
+        return(0);
 }
