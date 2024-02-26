131080f18c33f62a1c2d1179f4cc4461f473a7a6
psychohight <7298@holbertonstudents.com>
2024-02-26 17:33:22 +0100

    10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -9,5 +9,23 @@
 
 void print_triangle(int size)
 {
-    
+    int i, j, x;
+
+		if (size > 0)
+		{
+
+			for (i = 1; i <= size; i++)
+			{
+				for (j = 0; j <= (size - i - 1); j++)
+				    _putchar(' ');
+			
+            for (x = 0; x <= i - 1; x++)
+			    _putchar('#');
+
+		    _putchar('\n');
+			}
+	}
+	else
+		_putchar('\n');    
+
 }
