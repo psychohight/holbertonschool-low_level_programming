56e23738ef117878b84f4a88e03b2780185291cd
psychohight <7298@holbertonstudents.com>
2024-02-26 13:20:50 +0100

    5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -9,5 +9,19 @@
 
 void more_numbers(void)
 {
-    
+    int i;
+	int j;
+
+	for (j = 0; j <= 9; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+			{
+				_putchar(i / 10 + '0');
+			}
+			_putchar(i % 10 + '0');
+		}
+		_putchar('\n');
+	}
 }
