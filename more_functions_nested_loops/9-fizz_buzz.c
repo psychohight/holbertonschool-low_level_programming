192750a538b8c6059f3d14d72752d612d78483a0
psychohight <7298@holbertonstudents.com>
2024-02-26 16:25:44 +0100

    9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -18,7 +18,7 @@ int print_fizzbuzz (void)
         else if (n % 5 == 0)
             printf("Buzz ");
 
-        else if (n % 15 == 0)
+        else if (n % 3 == 0 && n % 5 == 0)
             printf("FizzBuzz ");
 
         else
