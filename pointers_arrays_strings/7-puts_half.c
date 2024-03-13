d3e94c10f68d60478b002ce3da12dfd05eb6db01
psychohight <7298@holbertonstudents.com>
2024-03-13 17:36:10 +0100

    7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -13,9 +13,14 @@ void puts_half(char *str)
     
     while (str[i] != '\0')
         i++;
-
-    for (i = i / 2; str[i] != '\0'; i++)
-        _putchar(str[i]);
-
+    
+    if ((i - 1) % 2 == 0)
+        for (i = (i - 1) / 2; str[i] != '\0'; i++)
+            _putchar(str[i]);
+    
+    else
+        for (i = i / 2; str[i] != '\0'; i++)
+            _putchar(str[i]);
+        
     _putchar('\n');
 }
