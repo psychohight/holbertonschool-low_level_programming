9725f01685e64b0b19351d9b719ce13bc597b75a
psychohight <7298@holbertonstudents.com>
2024-03-14 11:52:00 +0100

    0

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -9,5 +9,13 @@
 
 char *_strcat(char *dest, char *src)
 {
+    int i = 0, j = 0;
+ 
+    while (dest[i++])
+        j++;
     
+    for (i = 0; src[i] != '\0'; i++)
+        dest[j++] = src[i];
+
+    return(dest);
 }
