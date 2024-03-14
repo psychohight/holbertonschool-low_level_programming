845937b85c360db25430ccd7b0871f9ce4080766
psychohight <7298@holbertonstudents.com>
2024-03-14 12:26:23 +0100

    2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -8,5 +8,16 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-    
+    int i = 0, j = 0;
+
+    while (src[i++])
+        j++;
+
+    for (i = 0; src[i] && i < n; i++)
+        dest[i] = src[i];
+
+    for (i = j; i < n; i++)
+        dest[i] = '\0';
+
+    return(dest);
 }
