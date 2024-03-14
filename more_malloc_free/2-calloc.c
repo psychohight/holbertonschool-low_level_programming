ffb77cb115e1d64454cf490ab47164d7a086d599
psychohight <7298@holbertonstudents.com>
2024-03-14 17:23:37 +0100

    2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -10,5 +10,22 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-    
+    void *mem;
+	char *cons;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	mem = malloc(size * nmemb);
+
+	if (mem == NULL)
+		return (NULL);
+
+	cons = mem;
+
+	for (i = 0; i < (size * nmemb); i++)
+		cons[i] = '\0';
+
+	return (mem);
 }
