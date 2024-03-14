8bb8664fb9f67ab2803558557597d8ad830abd74
psychohight <7298@holbertonstudents.com>
2024-03-14 13:09:03 +0100

    5

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -6,7 +6,16 @@
  *
  * Return: Always 0.
  */
-char *string_toupper(char *)
+char *string_toupper(char *str)
 {
-    
+    int i = 0;
+
+    while (str[i])
+    {
+        if (str[i] >= 'a' && str[i] <= 'z')
+            str[i] -= 32;
+
+        i++;
+    }
+    return (str);
 }
