2cf542ea83a40e8168611c801af1226505855b36
psychohight <7298@holbertonstudents.com>
2024-03-14 14:16:59 +0100

    7

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -6,7 +6,19 @@
  *
  * Return: Always 0.
  */
-char *leet(char *)
+char *leet(char *str)
 {
-    
+    int i, j;
+    char *s1 = "aAeEoOtTlL";
+    char *s2 = "4433007711";
+
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        for (j = 0; s1[j] != '\0'; j++)
+        {
+            if (str[i] == s1[j])
+                str[i] = s2[j];
+        }
+    }
+    return (str);
 }
