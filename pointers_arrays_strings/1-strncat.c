0c42d0028bce981835bc7cbbdb037cded5d8fcf2
psychohight <7298@holbertonstudents.com>
2024-03-14 12:15:58 +0100

    1

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -8,5 +8,13 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
+    int i = 0, j = 0;
+ 
+    while (dest[i++])
+        j++;
     
+    for (i = 0; src[i] && i < n; i++)
+        dest[j++] = src[i];
+
+    return(dest);
 }
