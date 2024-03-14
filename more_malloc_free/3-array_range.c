fd7ae57b2f1fb2175b62e5a943fc29e3afb5ab9c
psychohight <7298@holbertonstudents.com>
2024-03-14 17:26:57 +0100

    3

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -10,5 +10,21 @@
  */
 int *array_range(int min, int max)
 {
-    
+    int *array;
+    int i, size;
+
+	if (min > max)
+		return (NULL);
+
+	size = max - min + 1;
+
+	array = malloc(sizeof(int) * size);
+
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		array[i] = min++;
+
+	return (array);
 }
