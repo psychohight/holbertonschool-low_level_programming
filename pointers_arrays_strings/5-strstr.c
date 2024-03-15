e1d2fcb8acff5e1d7e320945fc43b4d271425789
psychohight <7298@holbertonstudents.com>
2024-03-15 15:38:06 +0100

    5-6

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+    int i;
+
+    if (*needle == 0)
+        return (haystack);
+
+    while (*haystack)
+    {
+        i = 0;
+
+        if (haystack[i] == needle[i])
+        {
+            do
+            {
+                if (needle[i + 1] == '\0')
+                    return (haystack);
+            
+                i++;
+            }
+            while (haystack[i] == needle[i]);
+        }
+        haystack++;    
+    }
+    return('\0');
+}
