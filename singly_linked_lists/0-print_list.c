1b564b58c812b32f7c67ca050fdcdd5ed35d4ca1
psychohight <7298@holbertonstudents.com>
2024-04-02 17:22:38 +0200

    0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -10,5 +10,21 @@
  */
 size_t print_list(const list_t *h)
 {
-    
+    size_t nodes = 0;
+
+    while (h)
+    {
+        if (h->str == NULL)
+        {
+            printf("[0] (nil)\n");
+        }
+        else
+        {
+            printf("[%u] %s\n", h->len, h->str);
+        }
+        nodes++;
+        h = h->next;
+    }
+
+    return nodes;
 }
--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -13,14 +13,6 @@ typedef struct list_s
     struct list_s *next;
 } list_t;
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+int _putchar(char c);
+
+size_t print_list(const list_t *h);
