47f6bd33106cb43818975060b8cfd7c1945f8d63
psychohight <7298@holbertonstudents.com>
2024-04-02 14:07:14 +0200

    3

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -5,7 +5,20 @@
  *
  * Return: Always 0.
  */
-int int_index(int *array, int size, int (*cmp)(int))
+int (*get_op_func(char *s))(int, int)
 {
+    op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
 
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+		i++;
+
+	return (ops[i].f);
 }
