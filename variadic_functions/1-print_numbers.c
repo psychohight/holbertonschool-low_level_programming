7ee96df30aced9ff15b7fd2d2bfaa0606e3f5a94
psychohight <7298@holbertonstudents.com>
2024-04-03 14:34:46 +0200

    1-2

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -8,5 +8,21 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-    
+    va_list nums;
+    unsigned int i;
+
+    va_start(nums, n);
+
+    for (i = 0; i < n; i++)
+    {
+        printf("%d", va_arg(nums, int));
+        if (separator != NULL && i < n - 1)
+        {
+            printf("%s", separator);
+        }
+    }
+
+    va_end(nums);
+
+    printf("\n");
 }
--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -8,5 +8,27 @@
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-    
+    va_list strings;
+    unsigned int i;
+    char *str;
+
+    va_start(strings, n);
+
+    for (i = 0; i < n; i++)
+    {
+        str = va_arg(strings, char*);
+        if (str != NULL)
+            printf("%s", str);
+        else
+            printf("(nil)");
+
+        if (separator != NULL && i < n - 1)
+        {
+            printf("%s", separator);
+        }
+    }
+
+    va_end(strings);
+
+    printf("\n");
 }
